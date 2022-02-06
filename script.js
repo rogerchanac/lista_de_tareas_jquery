bcdf03233f45d5e650428b5261d45b54e13764e4
rogerchanac007 <armandoroger007@gmail.com>
2022-02-05 23:55:50 -0600

    Agregando el boton de eliminar al item creado

--- script.js
+++ script.js
@@ -7,7 +7,13 @@ $(function(){
         //Creacion de un item nuevo para la lista con el valor del textarea
         var item = $("<li></li>").text(txt_tarea.val())
         item.addClass("list-group-item")
+        //Creamos un boton para poder Eliminar la tarea
+        var btn_eliminar = $('<button class="btn btn-danger"></button>')
+        btn_eliminar.text("Eliminar")
+        item.append(btn_eliminar)
+        btn_eliminar.addClass("btn-eliminar")
         //agregamos el item a la lista
         lista_tareas.append(item)
+
     })
 });
